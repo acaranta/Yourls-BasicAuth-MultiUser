b812844f4fb9c3d4e9d493969ea8f87469c05039
acaranta <arthur+apps.git@caranta.com>
2013-05-07 15:31:27 +0200

    corrected path

--- multi-user-apacheauth/mufunctions.php
+++ multi-user-apacheauth/mufunctions.php
@@ -212,7 +212,7 @@ ROW;
 }
 
 function muAdminUrl($page = '') {
-	$admin = YOURLS_SITE . '/user/plugins/multi-user/' . $page;
+	$admin = YOURLS_SITE . '/user/plugins/multi-user-apacheauth/' . $page;
 	return yourls_apply_filter( 'admin_url', $admin, $page );
 }
 
